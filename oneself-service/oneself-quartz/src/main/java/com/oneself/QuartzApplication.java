6252fb9fb28afc3406e0e2ece332e32df1cce13c
liuhuan <crane0927@163.com>
2024-12-31 15:56:06 +0800

    refactor(oneself-quartz): 移除 QuartzApplication 中未使用的日志依赖
    - 删除了 lombok.extern.slf4j.Slf4j 注解
    - 移除了未使用的 import 语句

--- oneself-service/oneself-quartz/src/main/java/com/oneself/QuartzApplication.java
+++ oneself-service/oneself-quartz/src/main/java/com/oneself/QuartzApplication.java
@@ -1,13 +1,11 @@
 package com.oneself;
 
 import com.oneself.utils.ApplicationStartupUtils;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
 public class QuartzApplication {
