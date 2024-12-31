7818a6a89db81862e10310ffc5182d3896f48bb4
liuhuan <crane0927@163.com>
2024-12-31 14:50:40 +0800

    feat(utils): 添加应用程序启动配置工具类
    - 新增 ApplicationStartupUtils 类，用于打印应用程序启动信息
    - 实现了打印本机 IP 地址、项目接口路径和 Swagger 文档路径的功能

---
+++ oneself-common/src/main/java/com/oneself/utils/ApplicationStartupUtils.java
@@ -0,0 +1,61 @@
+package com.oneself.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+/**
+ * @author liuhuan
+ * date 2024/12/31
+ * packageName com.oneself.utils
+ * className ApplicationStartupUtils
+ * description 应用程序启动配置工具类
+ * version 1.0
+ */
+@Slf4j
+public class ApplicationStartupUtils {
+    /**
+     * 打印启动信息
+     *
+     * @param env Spring 环境变量
+     */
+    public static void printStartupInfo(Environment env) {
+        // 获取本机 IP 地址
+        String ip = Optional.ofNullable(getLocalHostAddress()).orElse("localhost");
+
+        // 获取端口号和上下文路径
+        String port = env.getProperty("server.port", "8080");
+        String contextPath = Optional.ofNullable(env.getProperty("server.servlet.context-path")).orElse("");
+        String applicationName = Optional.ofNullable(env.getProperty("spring.application.name")).orElse("oneself-xxx");
+
+        // 构建 URL 路径
+        String baseUrl = String.format("http://%s:%s%s", ip, port, contextPath);
+        String swaggerUrl = baseUrl + "/doc.html";
+
+        // 打印启动信息
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "{} 服务启动成功:\n\t" +
+                        "本机 IP 地址：{}\n\t" +
+                        "项目接口路径：{}\n\t" +
+                        "Swagger 文档：{}\n" +
+                        "----------------------------------------------------------",
+                applicationName, ip, baseUrl, swaggerUrl);
+    }
+
+    /**
+     * 获取本机 IP 地址
+     *
+     * @return 本机 IP 地址
+     */
+    private static String getLocalHostAddress() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.error("无法获取本机 IP 地址", e);
+            return null;
+        }
+    }
+}
