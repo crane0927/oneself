a3cf9687368a34bc1a04d6da2cd4288efe3b23ad
liuhuan <crane0927@163.com>
2025-01-02 21:40:13 +0800

    feat(oneself-demo): 添加 Elasticsearch 集群信息获取功能 - 在 DemoController 中添加 getElasticsearchClusterInfo 方法，用于获取 Elasticsearch 集群名称 - 在 application-dev.yml 中添加 Elasticsearch 相关配置 - 引入 ElasticsearchUtils 工具类，用于创建和管理 Elasticsearch 客户端

--- oneself-service/oneself-demo/src/main/java/com/oneself/controller/DemoController.java
+++ oneself-service/oneself-demo/src/main/java/com/oneself/controller/DemoController.java
@@ -1,16 +1,17 @@
 package com.oneself.controller;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.InfoResponse;
 import com.oneself.annotation.RequireLogin;
 import com.oneself.model.dto.DemoDTO;
 import com.oneself.model.vo.DemoVO;
 import com.oneself.model.vo.ResponseVO;
+import com.oneself.utils.ElasticsearchUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author liuhuan
@@ -27,11 +28,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping({"/demo"})
 public class DemoController {
 
+    private final ElasticsearchUtils elasticsearchUtils;
+
+    @Autowired
+    public DemoController(ElasticsearchUtils elasticsearchUtils) {
+        this.elasticsearchUtils = elasticsearchUtils;
+    }
+
     @ApiOperation(value = "你好 xxx")
     @PostMapping({"/say/hello"})
     public ResponseVO<DemoVO> sayHello(@RequestBody DemoDTO dto) {
         log.info("hello {}", dto.getName());
         DemoVO demoVO = new DemoVO("hello " + dto.getName());
         return ResponseVO.success(demoVO);
+    }
+
+    @ApiOperation(value = "Elasticsearch 集群名称获取")
+    @GetMapping({"/get/elasticsearch/cluster/info"})
+    public ResponseVO<DemoVO> getElasticsearchClusterInfo() {
+        ElasticsearchClient client = elasticsearchUtils.getClient();
+        try {
+            InfoResponse info = client.info();
+            log.info("Elasticsearch 集群名称: {}", info.clusterName());
+            DemoVO demoVO = new DemoVO(info.clusterName());
+            return ResponseVO.success(demoVO);
+        } catch (Exception e) {
+            log.error("Elasticsearch 集群名称获取异常: {}", e.getMessage());
+            return ResponseVO.failure("Elasticsearch 集群名称获取异常: " + e.getMessage());
+        }
+
     }
 }
