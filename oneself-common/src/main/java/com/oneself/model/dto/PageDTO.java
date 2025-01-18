2120278823790577630c7c7ef67b110d2a1a256c
liuhuan <crane0927@163.com>
2025-01-18 16:47:45 +0800

    refactor(oneself-common): 更新 PageDTO 类中字段的 Schema 注解
    - 为 condition 和 pagination 字段添加 requiredMode = Schema.RequiredMode.REQUIRED
    - 为 Pagination 类中的 pageNum 和 pageSize 字段添加 requiredMode = Schema.RequiredMode.REQUIRED
    - 移除 Pagination 类中 sort 字段的 example 属性

--- oneself-common/src/main/java/com/oneself/model/dto/PageDTO.java
+++ oneself-common/src/main/java/com/oneself/model/dto/PageDTO.java
@@ -19,18 +19,18 @@ public class PageDTO<T> implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
-    @Schema(description = "查询条件")
+    @Schema(description = "查询条件", requiredMode = Schema.RequiredMode.REQUIRED)
     private T condition;
-    @Schema(description = "分页信息")
+    @Schema(description = "分页信息", requiredMode = Schema.RequiredMode.REQUIRED)
     private Pagination pagination;
 
     @Data
     public static class Pagination {
-        @Schema(description = "页码", example = "1")
+        @Schema(description = "页码", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
         private Long pageNum = 1L;
-        @Schema(description = "页面大小", example = "50")
+        @Schema(description = "页面大小", example = "50", requiredMode = Schema.RequiredMode.REQUIRED)
         private Long pageSize = 50L;
-        @Schema(description = "排序字段", example = "createTime")
+        @Schema(description = "排序字段")
         private String sort;
     }
 }
