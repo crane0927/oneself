9855087aa3a486a0866b931bf0c0a53ce0421c5f
liuhuan <crane0927@163.com>
2025-01-20 12:02:15 +0800

    feat(oneself-system): 为 StatusEnum 添加从值创建枚举的方法
    - 在 StatusEnum 类中添加了 @JsonCreator 注解
    - 实现了 fromValue 方法，可以根据字符串值或名称创建 StatusEnum 实例
    - 如果传入的值无效，会抛出 IllegalArgumentException 异常

--- oneself-service/oneself-system/src/main/java/com/oneself/common/model/enums/StatusEnum.java
+++ oneself-service/oneself-system/src/main/java/com/oneself/common/model/enums/StatusEnum.java
@@ -1,5 +1,6 @@
 package com.oneself.common.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 
 /**
@@ -21,5 +22,18 @@ public enum StatusEnum {
     StatusEnum(Integer code, String desc) {
         this.code = code;
         this.desc = desc;
+    }
+
+    @JsonCreator
+    public static StatusEnum fromValue(String value) {
+        if (value == null || value.isEmpty()) {
+            return null;
+        }
+        for (StatusEnum status : StatusEnum.values()) {
+            if (String.valueOf(status.code).equals(value) || status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("StatusEnum 的值无效: " + value);
     }
 }
