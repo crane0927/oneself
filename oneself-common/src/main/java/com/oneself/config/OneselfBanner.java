8face632461aa60aac9210095fd88b7eb032cf5c
liuhuan <crane0927@163.com>
2025-01-23 10:13:58 +0800

    feat(oneself-common): 添加自定义启动 banner 类
    - 新增 OneselfBanner 类实现自定义启动图标和信息
    - 在 ApplicationStartupUtils 中使用 OneselfBanner 替换原有的 System.out 打印- 优化了启动信息的展示格式，增加了 Swagger 文档路径

--- oneself-common/src/main/java/com/oneself/config/OneselfBanner.java
+++ oneself-common/src/main/java/com/oneself/config/OneselfBanner.java
@@ -1,37 +1,33 @@
-package com.oneself.utils;
+package com.oneself.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.core.env.Environment;
 
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Optional;
 
-import static java.lang.System.out;
-
 /**
  * @author liuhuan
- * date 2024/12/31
- * packageName com.oneself.utils
- * className ApplicationStartupUtils
- * description 应用程序启动配置工具类
+ * date 2025/1/23
+ * packageName com.oneself.config
+ * className OneselfBanner
+ * description 自定义 banner 输出，目前被 banner.txt 覆盖
  * version 1.0
  */
 @Slf4j
-public class ApplicationStartupUtils {
-    /**
-     * 打印启动信息
-     *
-     * @param env Spring 环境变量
-     */
-    public static void printStartupInfo(Environment env) {
+public class OneselfBanner implements Banner {
+    @Override
+    public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
         // 获取本机 IP 地址
         String ip = Optional.ofNullable(getLocalHostAddress()).orElse("localhost");
 
         // 获取端口号和上下文路径
-        String port = env.getProperty("server.port", "8080");
-        String contextPath = Optional.ofNullable(env.getProperty("server.servlet.context-path")).orElse("");
-        String applicationName = Optional.ofNullable(env.getProperty("spring.application.name")).orElse("oneself-xxx");
+        String port = environment.getProperty("server.port", "8080");
+        String contextPath = Optional.ofNullable(environment.getProperty("server.servlet.context-path")).orElse("");
+        String applicationName = Optional.ofNullable(environment.getProperty("spring.application.name")).orElse("oneself");
 
         // 构建 URL 路径
         String baseUrl = String.format("http://%s:%s%s", ip, port, contextPath);
@@ -39,6 +35,9 @@ public class ApplicationStartupUtils {
 
         // 打印启动信息
         out.printf("""
+                                                  ┓┏
+                                         ┏┓┏┓┏┓┏┏┓┃╋
+                                         ┗┛┛┗┗ ┛┗ ┗┛  (v1.0.0)
                 ***************************************************************
                     %s 服务启动成功:
                     本机 IP 地址：%s
@@ -61,4 +60,5 @@ public class ApplicationStartupUtils {
             return null;
         }
     }
+
 }
