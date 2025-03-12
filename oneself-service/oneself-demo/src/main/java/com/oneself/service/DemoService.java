cd993c1284f038d9c2894e9ff13f564e8e5f50e3
liuhuan <crane0927@163.com>
2025-03-12 14:41:32 +0800

    feat(oneself-demo): 添加示例服务接口及其实现类 - 新增 DemoService 接口，用于演示示例服务 - 实现 DemoService 接口，提供线程安全的缓存操作方法 - 使用 ConcurrentHashMap 和 CopyOnWriteArrayList 实现线程安全的数据存储 - 添加数据获取方法，支持从缓存和数据库中获取数据

---
+++ oneself-service/oneself-demo/src/main/java/com/oneself/service/DemoService.java
@@ -0,0 +1,12 @@
+package com.oneself.service;
+
+/**
+ * @author liuhuan
+ * date 2025/3/12
+ * packageName com.oneself.service
+ * interfaceName DemoService
+ * description 样例接口
+ * version 1.0
+ */
+public interface DemoService {
+}
