ce845d4f7479bef0a9703bf6f9a477519d141435
liuhuan <crane0927@163.com>
2025-04-17 10:11:51 +0800

    feat(oneself-ai): 添加 Nacos 配置并启用 Feign 和 Discovery
    - 在 AiApplication 中启用 Feign 客户端和 Discovery 客户端
    - 添加 Nacos 配置到 application-dev.yml
    - 在 pom.xml 中添加 Nacos 相关依赖
    - 新增 oneself-ai-api 模块
    - 调整 oneself-demo 依赖结构

--- oneself-service/oneself-ai/src/main/java/com/oneself/AiApplication.java
+++ oneself-service/oneself-ai/src/main/java/com/oneself/AiApplication.java
@@ -4,18 +4,19 @@ import com.oneself.utils.ApplicationStartupUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.validation.annotation.Validated;
 
-//@EnableFeignClients
-//@EnableDiscoveryClient
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @Validated
 public class AiApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext application = SpringApplication.run(AiApplication.class, args);
-		// 使用工具类 打印启动信息
 		ApplicationStartupUtils.printStartupInfo(application.getEnvironment());
 	}
 
